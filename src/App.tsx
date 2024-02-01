33601cd6668ffa426ea0057c8c01e68ef0faacad
unknown <chibukhashviligiorgi@gmail.com>
2024-02-01 22:50:36 +0100

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { Header } from "./components/Header";
 import { MapContainer } from "./components/MapContainer";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Routes>
-        <Route path="/" element={<Navigate to="/europe" />} />
-        <Route path="/europe" element={<MapContainer />} />
-      </Routes>
+      <MapContainer />
     </BrowserRouter>
   );
 }
